7b69d867b439bbe7c53487df8e5f9159e6331da7
Cakindan <farhan0dzaky3ananda@gmail.com>
2021-09-28 21:18:24 +0700

    Update

--- functions/karyawan/update.js
+++ functions/karyawan/update.js
@@ -4,7 +4,16 @@
  */
 
 export default function (app, db) {
-    app.put("", function (request, response) {
-        // Put something here
-    });
-}
+    app.put("/karyawan", function (request, response) {
+        const { id, nama, tahun_masuk, jabatan } = request.body;
+
+        db.run('update entities set (id, nama, tahun_masuk, jabatan) VALUES (?, ?, ?, ?)', [id, nama, tahun_masuk, jabatan], function(err) 
+        {
+            if (err) {
+             logger.winston.error("Error updating");
+            } else {
+             res.status(200).json({ status: 'success', message: 'Updated' });
+            }
+        });
+    }
+    )}
