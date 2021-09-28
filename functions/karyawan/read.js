898421b799fd8da4128c995d5bf534faf72e83a4
saskia-dwi-ulfah <sdwiulfah@gmail.com>
2021-09-28 18:10:29 +0700

    Code for read karyawan

--- functions/karyawan/read.js
+++ functions/karyawan/read.js
@@ -3,8 +3,29 @@
  *  @param {import("sqlite3").Database} db
  */
 
-export default function (app, db) {
-    app.get("", function (request, response) {
-        // Put something here
+ export default function (app, db) {
+    app.get("/karyawan", function (request, response) {
+        db.all("SELECT * FROM karyawan", function (err, result) {
+            if (err) {
+                response.status(400).end();
+                return;
+            }
+
+            response.status(200).json(result);
+        });
     });
+
+    app.get("/karyawan/:id", function (request, response) {
+        const { id } = request.params;
+
+        db.all("SELECT * FROM karyawan WHERE id=?", id, function (err, result) {
+            if (err) {
+                response.status(400).end();
+                return;
+            }
+
+            response.status(200).json(result);
+        });
+    })
 }
+
