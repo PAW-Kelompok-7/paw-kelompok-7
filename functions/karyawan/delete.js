59bf81d1e07e533b77b4b6b1ea51e20f6d2291aa
pipiets <65380284+pipiets@users.noreply.github.com>
2021-09-28 23:26:12 +0700

    Add delete function in karyawan

--- functions/karyawan/delete.js
+++ functions/karyawan/delete.js
@@ -4,7 +4,16 @@
  */
 
 export default function (app, db) {
-    app.delete("", function (request, response) {
-        // Put something here
+    app.delete("/karyawan/:id", function (request, response) {
+        const { id, nama, tahun_masuk, jabatan } = request.body;
+
+        db.run('DELETE FROM karyawan WHERE id = ' + req.params.id, karyawan, function(err, result){
+            if (err) {
+                req.flash('error', err)
+            } else {
+                req.flash('success', 'Karyawan deleted successfully! id = ' + req.params.id)
+            }
+        });
+    });
     });
 }
