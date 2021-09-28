2a7ad1df20924706efb7753cbe7d11f469659541
Farrel R <rafirizqy12@gmail.com>
2021-09-28 22:06:54 +0700

    Fix some db syntaxes

--- db-setup.js
+++ db-setup.js
@@ -1,34 +1,31 @@
-import sqlite3 from "sqlite3";
+import sqlite3Stock from "sqlite3";
+
+const sqlite3 = sqlite3Stock.verbose();
 
 const db = new sqlite3.Database("./restoran.db");
 
 db.serialize(
     function () {
         db.run(`CREATE TABLE IF NOT EXISTS menu (
-            nomor       INTEGER         PRIMARY KEY,
-            nama        VARCHAR(255)    NOT NULL,
-            harga_rp    INT             NOT NULL
+            nomor           INTEGER         PRIMARY KEY,
+            nama            VARCHAR(255)    NOT NULL,
+            harga_rp        INT             NOT NULL
         )`);
-        db.run(`CREATE TABLE IF NOT EXISTS jabatan_karyawan (
-            index           INTEGER         PRIMARY KEY,
-            nama_jabatan    VARCHAR(255)    NOT NULL
+
+        db.run(`CREATE TABLE IF NOT EXISTS jabatan (
+            kode            INTEGER         PRIMARY KEY,
+            nama            VARCHAR(255)    NOT NULL
         )`);
+
         db.run(`CREATE TABLE IF NOT EXISTS karyawan (
-            id          INTEGER         PRIMARY KEY,
-            nama        VARCHAR(255)    NOT NULL,
-            tahun_masuk INT UNSIGNED    NOT NULL,
-            jabatan     VARCHAR(50)     NOT NULL,
-            FOREIGN KEY (jabatan) REFERENCES jabatan_karyawan(nama_jabatan)
+            id              INTEGER         PRIMARY KEY,
+            nama            VARCHAR(255)    NOT NULL,
+            tahun_masuk     INT UNSIGNED    NOT NULL,
+            jabatan         VARCHAR(50)     NOT NULL,
+            FOREIGN KEY (jabatan) REFERENCES jabatan(nama)
         )`);
-    },
-    function () {
-        db.run(`INSERT INTO jabatan_karyawan (
-            nama_jabatan
-        ) VALUES (
-            "manajer",
-            "pelayan",
-            "chef"
-        )`);
+
+        db.run(`INSERT INTO jabatan (nama) VALUES ("manajer"), ("pelayan"), ("chef")`);
     }
 );
 
